8d229bf78dd6623de059b864a90e8f9b6d5102df
DiogoFiuza <difipalm@gmail.com>
2022-04-30 13:56:27 -0300

    Feat: Criando controller

---
+++ app/backend/src/controllers/ColetaController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import ColetaService from '../services/ColetaService';
+
+class ColetaController {
+  constructor(private coletaService = new ColetaService()) { }
+
+  format = 'Error in the format data';
+  internalError = 'Internal server error';
+
+  public getDatas = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const datas = await this.coletaService.getDatas();
+
+      return res.status(200).send(datas);
+    } catch (err: unknown) {
+      return res.status(500).send({ message: this.internalError });
+    }
+  };
+
+  public create = async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const data = await this.coletaService.createData(req.body);
+      return res.status(201).send(data);
+    } catch (err: unknown) {
+      return res.status(500).send({ message: this.format });
+    }
+  };
+}
+
+export default ColetaController;
